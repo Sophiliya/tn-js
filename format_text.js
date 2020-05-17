ce50f8ae59bcc30064c6f8543cb24998c042e79a
sophiliya <sophiliya@gmail.com>
2020-05-17 22:38:24 +0600

    Fix after review

--- format_text.js
+++ format_text.js
@@ -1,16 +1,20 @@
-const lineLength = 80;
+const lineLength = 9;
 
 let text = prompt('Enter text:', '');
-let lines = [];
+let words = text.split(' ');
+let linesAll = [];
+let line = words[0];
 
-for(let i = 0; i < text.length; i += lineLength) {
-	lines.push(text.slice(i, i + lineLength));
+for(let i = 1; i < words.length; i++) {
+	if ((line + ' ' + words[i]).length < lineLength) {
+		line += ' ' + words[i];
+	} else {
+		linesAll.push(line + '\n');
+		line = words[i];
+	}
+
+	if (i === words.length - 1) { linesAll.push(line + '\n') }
 }
 
-let newText = lines.join('\n');
-
-console.log(newText)
-
-
-
-
+let newText = linesAll.join('');
+console.log(newText);
